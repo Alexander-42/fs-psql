94d67353f3f863a8a79e34c97ff2511628461945
Alexander Oiling <alexander.oiling@gmail.com>
2026-06-26 17:03:13 +0300

    More fix

--- util/config.js
+++ util/config.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 module.exports = {
   DATABASE_URL: process.env.TESTING = true ? process.env.TEST_DATABASE_URL : this.process.env.DATABASE_URL,
   PORT: process.env.PORT || 3001,
-  SECRET: process.env.JSON_WEBTOKEN_SECRET,
+  SECRET: process.env.SECRET || process.env.JSON_WEBTOKEN_SECRET,
 }

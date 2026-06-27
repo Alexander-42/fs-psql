e4866fc6ba0f97a58efead1901ee4998ebf7a8ab
Alexander Oiling <alexander.oiling@gmail.com>
2026-06-27 19:09:42 +0300

    Fixed workflow resulting to undefined TEST_DATABASE_URL by defaulting to TEST_DATABASE_URL in config

--- util/config.js
+++ util/config.js
@@ -4,7 +4,7 @@ module.exports = {
   DATABASE_URL:
     process.env.TESTING === 'true'
       ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
+      : process.env.DATABASE_URL || process.env.TEST_DATABASE_URL,
   PORT: process.env.PORT || 3001,
   SECRET: process.env.SECRET || process.env.JSON_WEBTOKEN_SECRET,
 }
